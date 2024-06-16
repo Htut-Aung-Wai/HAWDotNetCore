552ceb1d64eb8e54ca0d3b5ca65824fdbd1ff05f
Htut Aung Wai <htutaungwai333@gmail.com>
2024-06-16 15:46:43 +0630

    Dapper CRUD

--- HAWDotNetCore.ConsoleApp/DapperExample.cs
+++ HAWDotNetCore.ConsoleApp/DapperExample.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -14,12 +15,16 @@ namespace HAWDotNetCore.ConsoleApp
         public void Run()
         {
             Read();
+            //Edit(11);
+            //Create("htut13","htut13","htut13");
+            //Update(1012, "htut15", "htut15", "htut15");
+            //Delete(1011);
         }
 
-        public void Read()
+        private void Read()
         {
             using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
-            List<BlogDTO> lst = db.Query<BlogDTO>("select * from tbl_blog").ToList(); 
+            List<BlogDTO> lst = db.Query<BlogDTO>("select * from tbl_blog").ToList();  //Dapper
             foreach (BlogDTO item in lst)
             {
                 Console.WriteLine(item.BlogID);
@@ -30,6 +35,73 @@ namespace HAWDotNetCore.ConsoleApp
             }
            
 
+
+        }
+
+        private void Edit(int id)
+        {
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            var item=db.Query<BlogDTO>("select * from tbl_blog where blogid=@Blogid",new BlogDTO { BlogID=id}).FirstOrDefault();
+            if(item is null)
+            {
+                Console.WriteLine("No data found.");
+                return;
+            }
+            Console.WriteLine(item.BlogID);
+            Console.WriteLine(item.BlogTitle);
+            Console.WriteLine(item.BlogAuthor);
+            Console.WriteLine(item.BlogContent);
+            Console.WriteLine("______________________");
+        }
+
+        private void Create(string title,string author,string content)
+        {
+
+            var item = new BlogDTO
+            {
+
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+
+            };
+            string query = @"INSERT INTO [dbo].[Tbl_Blog] ([BlogTitle],[BlogAuthor],[BlogContent]) VALUES(@BlogTitle,@BlogAuthor,@BlogContent)";
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            int result=db.Execute(query,item);
+            string message = result > 0 ? "Saving Successful" : "Saving Fail";
+            Console.WriteLine(message);
+        }
+
+        private void Update(int id,string title, string author, string content)
+        {
+            var item = new BlogDTO
+            {
+                BlogID=id,
+                BlogTitle = title,
+                BlogAuthor = author,
+                BlogContent = content
+
+            };
+            string query = @"UPDATE [dbo].[Tbl_Blog] SET [BlogTitle] = @BlogTitle,[BlogAuthor] = @BlogAuthor,[BlogContent] = @BlogContent WHERE BlogId = @BlogID";
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+            string message = result > 0 ? "Update Successful" : "Update Fail";
+            Console.WriteLine(message);
+
+        }
+        private void Delete(int id)
+        {
+            var item = new BlogDTO
+            {
+                BlogID = id
+             
+
+            };
+            string query = @"delete from Tbl_Blog where BlogId = @BlogId";
+            using IDbConnection db = new SqlConnection(ConnectionStrings.SqlConnectionStringBuilder.ConnectionString);
+            int result = db.Execute(query, item);
+            string message = result > 0 ? "Delete Successful" : "Delete Fail";
+            Console.WriteLine(message);
 
         }
     }
